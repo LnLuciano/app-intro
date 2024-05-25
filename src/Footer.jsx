e9340a9bde9b0ec03622bc79b047732f24e59703
LnLuciano <lanasaluciano@gmail.com>
2024-05-25 19:54:01 -0300

    primer commit

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Footer() {
+    return (
+        <>
+            <p>Footer</p>
+        </>
+    )
+}
+
+export default Footer
